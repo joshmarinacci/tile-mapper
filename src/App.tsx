f1439aad835a83a9ad31fa2dc4d8084f97e623d3
Josh Marinacci <joshua@marinacci>
2023-04-27 16:34:30 -0700

    add inital code color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {HBox, VBox, DataGrid } from "josh_react_util";
+import {Point, Bounds} from "josh_js_util";
 import './App.css';
+
+export const PICO8 = [
+    '#000000',
+    '#1D2B53',
+    '#7E2553',
+    '#008751',
+    '#AB5236',
+    '#5F574F',
+    '#C2C3C7',
+    '#FFF1E8',
+    '#FF004D',
+    '#FFA300',
+    '#FFEC27',
+    '#00E436',
+    '#29ADFF',
+    '#83769C',
+    '#FF77A8',
+    '#FFCCAA',
+    'transparent',
+]
+
+type ImagePalette = string[]
+
+const palette:ImagePalette = PICO8
+
+
+function ListView(props: { renderer: any, data: any[], style:object }) {
+    const [selected, setSelected] = useState()
+    const Cell = props.renderer
+    return <ul className={'list-view'} style={props.style} >
+        {props.data.map((v,i) => {
+            return <li key={i}>{<Cell value={v} selected={selected} setSelected={setSelected}/>}</li>
+        })}
+    </ul>
+}
+
+
+function PaletteColorRenderer(props:{value:string, selected:any, setSelected:(value:any)=>void}) {
+    return <div style={{
+        backgroundColor:props.value,
+        width: '32px',
+        height: '32px',
+        borderWidth:'1px',
+        borderStyle: 'solid',
+        borderColor: props.selected===props.value?'white':'black',
+    }} onClick={()=>{
+        props.setSelected(props.value)
+    }
+    }> </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VBox>
+      <HBox>
+        <button>new</button>
+        <button>save</button>
+        <button>load</button>
+      </HBox>
+        <HBox>
+            <div>sheet list</div>
+            <ListView data={palette} renderer={PaletteColorRenderer} style={{
+                maxWidth:'300px'
+            }}/>
+            <div>pixel editor</div>
+        </HBox>
+    </VBox>
   );
 }
 
