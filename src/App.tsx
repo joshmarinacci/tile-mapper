1116a5f491ea43883c625e5d3342a14f9eeb814b
Josh Marinacci <joshua@marinacci>
2023-07-25 06:48:12 -0700

    add providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import {HBox, VBox} from "josh_react_util";
+import {
+    DialogContainer,
+    DialogContext,
+    DialogContextImpl,
+    HBox,
+    PopupContainer,
+    PopupContext,
+    PopupContextImpl,
+    VBox
+} from "josh_react_util";
 import {canvas_to_blob, forceDownloadBlob} from "josh_web_util";
 import './App.css';
 import {
@@ -39,7 +48,7 @@ EMPTY_DOC.setPalette(PICO8)
 
 const maparray = new ArrayGrid<EditableSprite>(20,20)
 
-function App() {
+function Main() {
     const [doc, setDoc] = useState<EditableDocument>(EMPTY_DOC)
     const [drawColor, setDrawColor] = useState<string>(palette[0])
     const [sheets, setSheets] = useState<EditableSheet[]>(EMPTY_DOC.getSheets())
@@ -67,7 +76,7 @@ function App() {
             })
         })
         input_element.click()
-        }
+    }
     const save_file = () => {
         let blob = jsonObjToBlob(doc.toJSONDoc())
         forceDownloadBlob(`${doc.getName()}.json`,blob)
@@ -93,39 +102,49 @@ function App() {
         doc.addEventListener(Changed, hand)
         return () => doc.removeEventListener(Changed, hand)
     },[doc]);
-  return (
-    <VBox>
-      <HBox >
-        <button>new</button>
-        <button onClick={save_file}>save</button>
-        <button onClick={load_file}>load</button>
-          <button onClick={export_png}>to PNG</button>
-          <button onClick={export_bmp}>to BMP</button>
-          <label>{doc.getName()}</label>
-      </HBox>
-        <div className={'main'}>
-            <SheetList sheet={sheet} setSheet={setSheet} doc={doc}/>
-            <div className={'pane'}>
-                <header>Tile Sheet</header>
-                {sheet&&<TileSheetView sheet={sheet} tile={tile} setTile={(t:EditableSprite)=> setTile(t)} palette={palette}/>}
+
+    return (
+        <VBox>
+            <HBox >
+                <button>new</button>
+                <button onClick={save_file}>save</button>
+                <button onClick={load_file}>load</button>
+                <button onClick={export_png}>to PNG</button>
+                <button onClick={export_bmp}>to BMP</button>
+                <label>{doc.getName()}</label>
+            </HBox>
+            <div className={'main'}>
+                <SheetList sheet={sheet} setSheet={setSheet} doc={doc}/>
+                <div className={'pane'}>
+                    <header>Tile Sheet</header>
+                    {sheet&&<TileSheetView sheet={sheet} tile={tile} setTile={(t:EditableSprite)=> setTile(t)} palette={palette}/>}
+                </div>
+                <div className={'pane'}>
+                    <header>Tile Info</header>
+                    {tile && <TileProperties tile={tile}/>}
+                </div>
+                <PaletteColorPickerPane drawColor={drawColor} setDrawColor={setDrawColor} palette={palette}/>
+                {tile && <PixelGridEditor
+                    selectedColor={palette.indexOf(drawColor)}
+                    setSelectedColor={(n)=> setDrawColor(palette[n])}
+                    image={tile} palette={palette}/>}
+                {!tile && <div>no tile selected</div>}
+                <div className={'pane'}>
+                    <header>Test</header>
+                    <TestMap tile={tile} mapArray={maparray}/>
+                </div>
             </div>
-            <div className={'pane'}>
-                <header>Tile Info</header>
-                {tile && <TileProperties tile={tile}/>}
-            </div>
-            <PaletteColorPickerPane drawColor={drawColor} setDrawColor={setDrawColor} palette={palette}/>
-            {tile && <PixelGridEditor
-                selectedColor={palette.indexOf(drawColor)}
-                setSelectedColor={(n)=> setDrawColor(palette[n])}
-                image={tile} palette={palette}/>}
-            {!tile && <div>no tile selected</div>}
-            <div className={'pane'}>
-                <header>Test</header>
-                <TestMap tile={tile} mapArray={maparray}/>
-            </div>
-        </div>
-    </VBox>
-  );
+        </VBox>
+    );
 }
 
+function App() {
+    return <DialogContext.Provider value={new DialogContextImpl()}>
+        <PopupContext.Provider value={new PopupContextImpl()}>
+            <Main/>
+            <PopupContainer/>
+            <DialogContainer/>
+        </PopupContext.Provider>
+    </DialogContext.Provider>
+}
 export default App;
