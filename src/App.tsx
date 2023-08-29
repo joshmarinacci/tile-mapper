42ab4601fd3f2c3a23386938bcaf9167ad3904c0
josh <joshua@marinacci.org>
2023-08-28 18:10:40 -0700

    switch to esbuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React, {useContext, useState} from 'react';
+import './App.css'
+
 import {
     DialogContainer,
     DialogContext,
     DialogContextImpl,
-    HBox,
     PopupContainer,
     PopupContext,
     PopupContextImpl,
-    Spacer,
-    VBox
-} from "josh_react_util";
-import {canvas_to_blob, forceDownloadBlob} from "josh_web_util";
-import './App.css';
+    Spacer
+} from "josh_react_util"
+import {canvas_to_blob, forceDownloadBlob} from "josh_web_util"
+import React, {useContext, useState} from 'react'
+
+import {EditableLabel} from "./common-components"
+import {MapModeView} from "./MapModeView"
 import {
     canvas_to_bmp,
     EditableDocument,
@@ -23,11 +25,9 @@ import {
     make_doc_from_json,
     PICO8,
     sheet_to_canvas
-} from "./model";
-import {NewDocDialog} from "./NewDocDialog";
-import {MapModeView} from "./MapModeView";
-import {EditableLabel} from "./common-components";
-import {TileModeView} from "./TileModeView";
+} from "./model"
+import {NewDocDialog} from "./NewDocDialog"
+import {TileModeView} from "./TileModeView"
 
 const EMPTY_DOC = new EditableDocument()
 {
@@ -42,9 +42,9 @@ EMPTY_DOC.setPalette(PICO8)
 
 
 const export_png = async (doc:EditableDocument) => {
-    for(let sheet of doc.getSheets()) {
+    for(const sheet of doc.getSheets()) {
         const can = sheet_to_canvas(sheet)
-        let blob = await canvas_to_blob(can)
+        const blob = await canvas_to_blob(can)
         forceDownloadBlob(`${doc.getName()}.${sheet.getName()}.png`,blob)
     }
 }
@@ -53,25 +53,25 @@ const export_bmp = async (doc:EditableDocument) => {
     const sheet = doc.getSheets()[0]
     const canvas = sheet_to_canvas(sheet)
     const rawData = canvas_to_bmp(canvas, doc.getPalette())
-    let blob = new Blob([rawData.data], {type:'image/bmp'})
+    const blob = new Blob([rawData.data], {type:'image/bmp'})
     forceDownloadBlob(`${sheet.getName()}.bmp`,blob)
 }
 
 const load_file = async ():Promise<EditableDocument> => {
-    let input_element = document.createElement('input')
+    const input_element = document.createElement('input')
     input_element.setAttribute('type','file')
     input_element.style.display = 'none'
     document.body.appendChild(input_element)
     return new Promise((res,rej)=>{
         input_element.addEventListener('change',() => {
-            console.log("user picked a file, we hope");
-            let files = input_element.files;
-            if(!files || files.length <= 0) return;
-            let file = files[0]
+            console.log("user picked a file, we hope")
+            const files = input_element.files
+            if(!files || files.length <= 0) return
+            const file = files[0]
             console.log(file)
             fileToJson(file).then(data => {
                 log("got the data",data)
-                let doc = make_doc_from_json(data)
+                const doc = make_doc_from_json(data)
                 log('doc iss',doc)
                 res(doc)
             })
@@ -85,7 +85,7 @@ function Main() {
     const [mode, setMode ]= useState('tiles')
     const dc = useContext(DialogContext)
     const save_file = () => {
-        let blob = jsonObjToBlob(doc.toJSONDoc())
+        const blob = jsonObjToBlob(doc.toJSONDoc())
         forceDownloadBlob(`${doc.getName()}.json`,blob)
     }
     const new_doc = () => {
@@ -97,7 +97,7 @@ function Main() {
                 <button onClick={new_doc}>new</button>
                 <button onClick={save_file}>save</button>
                 <button onClick={async ()=> {
-                    let doc = await load_file()
+                    const doc = await load_file()
                     setDoc(doc)
                 }}>load</button>
                 <button onClick={async () => await export_png(doc)}>to PNG</button>
@@ -110,7 +110,7 @@ function Main() {
             {mode === 'tiles' && <TileModeView doc={doc}/>}
             {mode === 'maps' && <MapModeView doc={doc}/>}
         </>
-    );
+    )
 }
 
 function App() {
@@ -122,4 +122,4 @@ function App() {
         </PopupContext.Provider>
     </DialogContext.Provider>
 }
-export default App;
+export default App
