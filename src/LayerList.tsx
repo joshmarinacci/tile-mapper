24fa32ef1e1e79f17b05e6d728638d02b7463864
josh <joshua@marinacci.org>
2023-09-22 14:16:05 -0700

    resize layer

--- src/LayerList.tsx
+++ src/LayerList.tsx
@@ -1,9 +1,11 @@
 import {Size} from "josh_js_util"
-import React from "react"
+import {DialogContext} from "josh_react_util"
+import React, {useContext} from "react"
 
 import {appendToList, PropsBase, useWatchProp} from "./base"
 import {GameMap, MapLayerType, TileLayer} from "./datamodel"
 import {ListView, ListViewDirection, ListViewRenderer} from "./ListView"
+import {ResizeLayerDialog} from "./ResizeLayerDialog"
 
 const LayerNameRenderer: ListViewRenderer<PropsBase<MapLayerType>> = (props: {
     value: PropsBase<MapLayerType>,
@@ -15,6 +17,7 @@ const LayerNameRenderer: ListViewRenderer<PropsBase<MapLayerType>> = (props: {
     }}><b>{props.value.getPropValue('name')}</b> <i>{props.value.getPropValue('type')}</i></div>
 }
 
+
 export function LayerList(props: {
     setSelectedLayer: (value: PropsBase<MapLayerType>) => void,
     map: GameMap,
@@ -22,6 +25,7 @@ export function LayerList(props: {
     layer: PropsBase<MapLayerType>
 }) {
 
+    const dm = useContext(DialogContext)
     useWatchProp(props.map,"layers")
     const add_tile_layer = () => {
         const layer = new TileLayer({name:'new tile layer', size: new Size(20,10), type:'tile-layer'})
@@ -54,16 +58,21 @@ export function LayerList(props: {
         props.map.setPropValue('layers',layers)
 
     }
+    const resize = () => {
+        if(props.layer instanceof  TileLayer) {}
+        dm.show(<ResizeLayerDialog layer={props.layer}/>)
+    }
 
     return <div className={'pane layer-list-view'}>
         <header>Layers</header>
         {props.editable &&
             <div className={'toolbar'}>
-                <button onClick={add_tile_layer}>add tile layer</button>
-                <button onClick={add_actor_layer}>add actor layer</button>
+                <button onClick={add_tile_layer}>+ tile layer</button>
+                <button onClick={add_actor_layer}>+ actor layer</button>
                 <button onClick={delete_layer}>del layer</button>
-                <button onClick={move_layer_up}>down</button>
-                <button onClick={move_layer_down}>up</button>
+                <button onClick={move_layer_up}>▼</button>
+                <button onClick={move_layer_down}>▲</button>
+                <button onClick={resize}>resize</button>
             </div>}
         <ListView selected={props.layer}
                   setSelected={props.setSelectedLayer}
---
+++ src/ResizeLayerDialog.tsx
@@ -0,0 +1,46 @@
+import {ArrayGrid, Size} from "josh_js_util"
+import {DialogContext} from "josh_react_util"
+import React, {useContext, useState} from "react"
+
+import {MapCell, TileLayer} from "./datamodel"
+
+enum ArrayGridResizeAlignment {
+    UPPER_LEFT,
+    CENTER
+}
+
+function resizeArrayGrid<T>(data: ArrayGrid<MapCell>, size: Size, alignment: ArrayGridResizeAlignment, empty: MapCell):ArrayGrid<T> {
+    const d2 = new ArrayGrid<T>(size.w,size.h)
+    d2.fill(n => {
+        if(data.isValidIndex(n)) return data.get(n)
+        return empty
+    })
+    return d2
+}
+
+export function ResizeLayerDialog(props: { layer: TileLayer }) {
+    const [width,setWidth] = useState(props.layer.getPropValue('size').w)
+    const [height,setHeight] = useState(props.layer.getPropValue('size').h)
+    const dc = useContext(DialogContext)
+    const doResize = () => {
+        const size = new Size(width,height)
+        const data = props.layer.getPropValue('data')
+        const data2:ArrayGrid<MapCell> = resizeArrayGrid(data,size, ArrayGridResizeAlignment.UPPER_LEFT, {tile:'unknown'} as MapCell)
+        props.layer.setPropValue('data',data2)
+        props.layer.setPropValue('size',size)
+        dc.hide()
+    }
+    return <div className={'dialog'}>
+        <header>resize layer</header>
+        <section className={'standard-form'}>
+            <label>width</label> <input type={'number'} value={width}
+                                        onChange={(e) => setWidth(parseInt(e.target.value))}/>
+            <label>height</label> <input type={'number'} value={height}
+                                         onChange={(e) => setHeight(parseInt(e.target.value))}/>
+        </section>
+        <footer>
+            <button onClick={() => dc.hide()}>cancel</button>
+            <button className={'primary'} onClick={doResize}>resize</button>
+        </footer>
+    </div>
+}
