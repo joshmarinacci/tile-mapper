8ca1496242e5b970622ec3f6b702927935330ae0
Josh Marinacci <josh@trunk.io>
2023-11-10 14:29:45 -0800

    add prop sheet for camera. make play update live

--- src/preview/PlayTest.tsx
+++ src/preview/PlayTest.tsx
@@ -1,105 +1,18 @@
-import { ArrayGrid } from "josh_js_util"
 import { DialogContext } from "josh_react_util"
 import React, { useContext, useEffect, useRef, useState } from "react"
-import {
-  ActorLayer as ACL,
-  PhysicsConstants,
-  Player,
-  RIGHT,
-  TilemapLayer,
-  TileReference,
-} from "retrogami-engine"
+import { PhysicsConstants } from "retrogami-engine"
 
 import { Icons } from "../common/common"
 import { ToggleButton } from "../common/common-components"
-import { GameState } from "../engine/gamestate"
-import { drawImage } from "../imageeditor/ImageEditorView"
-import { findActorForInstance } from "../mapeditor/ActorEditor"
+import { useWatchAllProps } from "../model/base"
 import { DocContext } from "../model/contexts"
-import { ActorLayer, GameMap, MapCell, TileLayer } from "../model/datamodel"
-import { GameDoc } from "../model/gamedoc"
+import { GameMap } from "../model/datamodel"
+import { PropSheet } from "../propsheet/propsheet"
 import { ActorDebugOverlay } from "./ActorDebugLayer"
 import { Anim } from "./Anim"
+import { generateGamestate } from "./generateGamestate"
 import { GridDebugOverlay } from "./GridDebugOverlay"
 import { ViewportDebugOverlay } from "./ViewportDebugOverlay"
-
-function generateGamestate(current: HTMLCanvasElement, doc: GameDoc, map: GameMap): GameState {
-  const gamestate = new GameState(current, doc)
-  // pre-cache all of the tiles
-  doc.getPropValue("sheets").forEach((sht) => {
-    sht.getPropValue("tiles").forEach((tile) => {
-      const can = doc.lookup_canvas(tile.getUUID())
-      if (can) {
-        gamestate.tileCache.addCachedTile(tile.getPropValue("name"), tile.getUUID(), {
-          name: tile.getPropValue("name"),
-          id: tile.getUUID(),
-          blocking: tile.getPropValue("blocking"),
-          canvas: can,
-        })
-      }
-    })
-  })
-  doc.getPropValue("canvases").forEach((img) => {
-    const canvas = document.createElement("canvas")
-    canvas.width = img.getPropValue("size").w
-    canvas.height = img.getPropValue("size").h
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
-    drawImage(doc, ctx, img, doc.getPropValue("palette"), 1)
-    gamestate.imageCache.addImage(img.getPropValue("name"), img.getUUID(), canvas)
-  })
-  // turn each layer of the map into a layer of the engine
-  map.getPropValue("layers").forEach((layer) => {
-    if (layer instanceof TileLayer) {
-      const tl = new TilemapLayer()
-      tl.type = "tilemap"
-      tl.name = layer.getPropValue("name")
-      tl.blocking = layer.getPropValue("blocking")
-      tl.wrapping = layer.getPropValue("wrapping")
-      tl.scrollSpeed = layer.getPropValue("scrollSpeed")
-      const size = layer.getPropValue("size")
-      tl.tiles = new ArrayGrid<TileReference>(size.w, size.h)
-      const editorCells = layer.getPropValue("data") as ArrayGrid<MapCell>
-      tl.tiles.fill((n) => {
-        if (editorCells.get(n)) return { uuid: editorCells.get(n).tile }
-        return { uuid: "unknown" }
-      })
-      gamestate.addLayer(tl)
-    }
-    if (layer instanceof ActorLayer) {
-      const actors = new ACL()
-      actors.blocking = true
-      gamestate.addLayer(actors)
-      layer.getPropValue("actors").forEach((inst) => {
-        const real_actor = findActorForInstance(inst, doc)
-        if (real_actor) {
-          const pos = inst.getPropValue("position")
-          const val: Player = {
-            bounds: real_actor.getPropValue("viewbox").add(pos),
-            hidden: false,
-            type: "player",
-            color: "blue",
-            tile: {
-              uuid: real_actor.getPropValue("sprite") as string,
-            },
-            name: inst.getPropValue("name"),
-            hitable: true,
-            vy: 0,
-            vx: 0,
-            standing: false,
-            dir: RIGHT,
-            opacity: 1.0,
-            originalPosition: pos,
-          }
-          actors.addActor(val)
-          if (real_actor.getPropValue("kind") === "player") {
-            gamestate.addPlayer(val)
-          }
-        }
-      })
-    }
-  })
-  return gamestate
-}
 
 export function PlayTest(props: { map: GameMap }) {
   const { map } = props
@@ -149,6 +62,7 @@ export function PlayTest(props: { map: GameMap }) {
     () => redraw(),
     [doc, zoom, showGrid, showViewport, showHidden, showActors, ref, showPhysics],
   )
+  useWatchAllProps(camera, () => redraw())
   useEffect(() => {
     if (playing) {
       anim.stop()
@@ -174,7 +88,7 @@ export function PlayTest(props: { map: GameMap }) {
       }}
     >
       <header>Play test</header>
-      <section>
+      <section className={"vbox"}>
         <div className={"toolbar"}>
           <label>Debug</label>
           <ToggleButton
@@ -213,12 +127,15 @@ export function PlayTest(props: { map: GameMap }) {
             text={"hidden"}
           />
         </div>
-        <canvas
-          ref={ref}
-          tabIndex={0}
-          width={(viewport.w + 5) * tileSize.w * zoom}
-          height={(viewport.h + 5) * tileSize.h * zoom}
-        ></canvas>
+        <div className={"hbox"}>
+          <canvas
+            ref={ref}
+            tabIndex={0}
+            width={(viewport.w + 5) * tileSize.w * zoom}
+            height={(viewport.h + 5) * tileSize.h * zoom}
+          ></canvas>
+          <PropSheet target={camera} collapsable={true} />
+        </div>
       </section>
       <footer>
         <button onClick={dismiss}>dismiss</button>
---
+++ src/preview/generateGamestate.ts
@@ -0,0 +1,90 @@
+import { ArrayGrid } from "josh_js_util"
+import { ActorLayer as ACL, Player, RIGHT, TilemapLayer, TileReference } from "retrogami-engine"
+
+import { GameState } from "../engine/gamestate"
+import { drawImage } from "../imageeditor/ImageEditorView"
+import { findActorForInstance } from "../mapeditor/ActorEditor"
+import { ActorLayer, GameMap, MapCell, TileLayer } from "../model/datamodel"
+import { GameDoc } from "../model/gamedoc"
+
+export function generateGamestate(
+  current: HTMLCanvasElement,
+  doc: GameDoc,
+  map: GameMap,
+): GameState {
+  const gamestate = new GameState(current, doc)
+  // pre-cache all of the tiles
+  doc.getPropValue("sheets").forEach((sht) => {
+    sht.getPropValue("tiles").forEach((tile) => {
+      const can = doc.lookup_canvas(tile.getUUID())
+      if (can) {
+        gamestate.tileCache.addCachedTile(tile.getPropValue("name"), tile.getUUID(), {
+          name: tile.getPropValue("name"),
+          id: tile.getUUID(),
+          blocking: tile.getPropValue("blocking"),
+          canvas: can,
+        })
+      }
+    })
+  })
+  doc.getPropValue("canvases").forEach((img) => {
+    const canvas = document.createElement("canvas")
+    canvas.width = img.getPropValue("size").w
+    canvas.height = img.getPropValue("size").h
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+    drawImage(doc, ctx, img, doc.getPropValue("palette"), 1)
+    gamestate.imageCache.addImage(img.getPropValue("name"), img.getUUID(), canvas)
+  })
+  // turn each layer of the map into a layer of the engine
+  map.getPropValue("layers").forEach((layer) => {
+    if (layer instanceof TileLayer) {
+      const tl = new TilemapLayer()
+      tl.type = "tilemap"
+      tl.name = layer.getPropValue("name")
+      tl.blocking = layer.getPropValue("blocking")
+      tl.wrapping = layer.getPropValue("wrapping")
+      tl.scrollSpeed = layer.getPropValue("scrollSpeed")
+      const size = layer.getPropValue("size")
+      tl.tiles = new ArrayGrid<TileReference>(size.w, size.h)
+      const editorCells = layer.getPropValue("data") as ArrayGrid<MapCell>
+      tl.tiles.fill((n) => {
+        if (editorCells.get(n)) return { uuid: editorCells.get(n).tile }
+        return { uuid: "unknown" }
+      })
+      gamestate.addLayer(tl)
+    }
+    if (layer instanceof ActorLayer) {
+      const actors = new ACL()
+      actors.blocking = true
+      gamestate.addLayer(actors)
+      layer.getPropValue("actors").forEach((inst) => {
+        const real_actor = findActorForInstance(inst, doc)
+        if (real_actor) {
+          const pos = inst.getPropValue("position")
+          const val: Player = {
+            bounds: real_actor.getPropValue("viewbox").add(pos),
+            hidden: false,
+            type: "player",
+            color: "blue",
+            tile: {
+              uuid: real_actor.getPropValue("sprite") as string,
+            },
+            name: inst.getPropValue("name"),
+            hitable: true,
+            vy: 0,
+            vx: 0,
+            standing: false,
+            dir: RIGHT,
+            opacity: 1.0,
+            originalPosition: pos,
+          }
+          actors.addActor(val)
+          if (real_actor.getPropValue("kind") === "player") {
+            gamestate.addPlayer(val)
+          }
+        }
+      })
+    }
+  })
+  return gamestate
+}
