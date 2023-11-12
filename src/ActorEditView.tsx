d392ce70f37c9e2de3e9bf524111f820a8355723
Josh Marinacci <josh@trunk.io>
2023-11-11 20:14:10 -0800

    make actor editor redraw properly

--- src/ActorEditView.tsx
+++ src/ActorEditView.tsx
@@ -4,50 +4,39 @@ import { IconButton } from "./common/common-components"
 import { Icons } from "./common/icons"
 import { drawImage } from "./imageeditor/ImageEditorView"
 import { Actor } from "./model/actor"
-import { useWatchAllProps, useWatchProp } from "./model/base"
+import { useWatchAllProps } from "./model/base"
 import { DocContext, StateContext } from "./model/contexts"
 import { strokeBounds } from "./util"
 
-function SpriteView(props: {
-  sprite: string
-  scale: number
-  onDraw: (ctx: CanvasRenderingContext2D) => void
-}) {
-  const doc = useContext(DocContext)
-  const ref = useRef<HTMLCanvasElement>(null)
-  useEffect(() => {
-    if (ref.current) {
-      const ctx = ref.current.getContext("2d") as CanvasRenderingContext2D
-      ctx.fillStyle = "white"
-      ctx.fillRect(0, 0, ref.current.width, ref.current.height)
-      const img = doc.getPropValue("canvases").find((img) => img.getUUID() === props.sprite)
-      if (img) {
-        drawImage(doc, ctx, img, doc.getPropValue("palette"), props.scale)
-      }
-      props.onDraw(ctx)
-    }
-  }, [props.sprite, props.scale])
-  return (
-    <div>
-      <canvas ref={ref} width={512} height={512} />
-    </div>
-  )
-}
-
 export function ActorEditView(props: { actor: Actor }) {
   const [zoom, setZoom] = useState(4)
-  const sprite = props.actor.getPropValue("sprite")
-  useWatchAllProps(props.actor)
-  useWatchProp(props.actor, "hitbox")
+  const spriteId = props.actor.getPropValue("sprite")
   const scale = Math.pow(2, zoom)
   const state = useContext(StateContext)
   const doc = useContext(DocContext)
+  const ref = useRef<HTMLCanvasElement>(null)
   const editSprite = () => {
-    const img = doc.getPropValue("canvases").find((img) => img.getUUID() === sprite)
+    const img = doc.getPropValue("canvases").find((img) => img.getUUID() === spriteId)
     if (img) {
       state.setSelectionTarget(img)
     }
   }
+  function redraw() {
+    if (ref.current) {
+      const ctx = ref.current.getContext("2d") as CanvasRenderingContext2D
+      ctx.fillStyle = "white"
+      ctx.fillRect(0, 0, ref.current.width, ref.current.height)
+      const img = doc.getPropValue("canvases").find((img) => img.getUUID() === spriteId)
+      if (img) drawImage(doc, ctx, img, doc.getPropValue("palette"), scale)
+      const view_bounds = props.actor.getPropValue("viewbox").scale(scale)
+      strokeBounds(ctx, view_bounds, "red", 3)
+      const hit_bounds = props.actor.getPropValue("hitbox").scale(scale)
+      strokeBounds(ctx, hit_bounds, "magenta", 3)
+    }
+  }
+  useEffect(() => redraw(), [spriteId, scale])
+  useWatchAllProps(props.actor, () => redraw())
+
   return (
     <>
       <div className={"tool-column"}> tool column </div>
@@ -57,18 +46,7 @@ export function ActorEditView(props: { actor: Actor }) {
           <IconButton onClick={() => setZoom(zoom + 1)} icon={Icons.Plus} tooltip={"zoom out"} />
           <button onClick={() => editSprite()}>edit sprite</button>
         </div>
-        {sprite && (
-          <SpriteView
-            sprite={sprite}
-            scale={scale}
-            onDraw={(ctx: CanvasRenderingContext2D) => {
-              const view_bounds = props.actor.getPropValue("viewbox").scale(scale)
-              strokeBounds(ctx, view_bounds, "red", 3)
-              const hit_bounds = props.actor.getPropValue("hitbox").scale(scale)
-              strokeBounds(ctx, hit_bounds, "magenta", 3)
-            }}
-          />
-        )}
+        <canvas ref={ref} width={512} height={512} />
       </div>
     </>
   )
