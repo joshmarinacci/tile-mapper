b01187115aabf6975f5aa74b17b4f383ff3b440a
Josh Marinacci <josh@trunk.io>
2023-12-10 15:57:54 -0800

    make toaster hide

--- src/ToasterView.tsx
+++ src/ToasterView.tsx
@@ -6,17 +6,31 @@ import { StateContext } from "./model/contexts"
 
 export function ToasterView(props: {}) {
   const state = useContext(StateContext)
+  const [visible, setVisible] = useState(false)
   const [message, setMessage] = useState("")
   useEffect(() => {
     const toaster = state.getPropValue("toaster")
     const handle = () => {
       console.log("a message happened", toaster.message)
+      setVisible(true)
       setMessage(toaster.message)
+      setTimeout(() => {
+        setVisible(false)
+      }, 1000)
     }
     toaster.on("append", handle)
     return () => {
       toaster.off("append", handle)
     }
   }, [])
-  return <div className={"toaster-view"}>{message}</div>
+  return (
+    <div
+      style={{
+        visibility: visible ? "visible" : "hidden",
+      }}
+      className={"toaster-view"}
+    >
+      {message}
+    </div>
+  )
 }
